456010e9c8f21d7ad3d283ff6af0fd3ae3e2c4ba
“jy8474np” <jy8474np@go.minnstate.edu>
2020-10-04 14:40:55 -0500

    add_new_artist function created.

--- db.py
+++ db.py
@@ -11,3 +11,13 @@ def create_artworks_table():
     with sqlite3.connect(database) as conn:
         conn.execute("CREATE TABLE IF NOT EXISTS artworks (artist_name TEXT, artwork_name TEXT, price REAL, available BOOLEAN (UNIQUE artwork_name COLLATE NOCASE))")
     conn.close()
+
+def add_new_artist():
+    new_artist_name = input("What is the name of the artist you would like to add? ") # Prompt useer for new artist's name
+    new_artist_email = input("Please enter the e-mail address of the artist you would like to add: ") # Prompt user for new artist's e-mail address
+    
+    with sqlite3.connect(database) as conn:
+        conn.execute(f"INSERT INTO artists VALUES (? , ?)", (new_artist_name , new_artist_email))
+    conn.close()
+
+
