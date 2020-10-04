bdc40f1862dabc7b4ca55812e805e028f5f75926
“jy8474np” <jy8474np@go.minnstate.edu>
2020-10-04 14:31:19 -0500

    database made global variable associated with database path.

--- db.py
+++ db.py
@@ -1,13 +1,13 @@
 import sqlite3 # Import SQLite3
 
-database = "art.sqlite3"
+database = "art.sqlite3" # For ease, make "database" a Global Variable always associated with "art.sqlite"
 
 def create_artists_table():
-    with sqlite3.connect("art.qlite3") as conn:
+    with sqlite3.connect(database) as conn:
         conn.execute("CREATE TABLE IF NOT EXISTS artists (artist_name TEXT, artist_email TEXT (UNIQUE artist_email COLLATE NOCASE))")
     conn.close()
 
 def create_artworks_table():
-    with sqlite3.connect("art.qlite3") as conn:
+    with sqlite3.connect(database) as conn:
         conn.execute("CREATE TABLE IF NOT EXISTS artworks (artist_name TEXT, artwork_name TEXT, price REAL, available BOOLEAN (UNIQUE artwork_name COLLATE NOCASE))")
     conn.close()
