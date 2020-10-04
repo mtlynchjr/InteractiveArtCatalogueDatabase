7e27b92adb17969d6b2dec508737c5ae4d1b00a7
“jy8474np” <jy8474np@go.minnstate.edu>
2020-10-04 14:28:09 -0500

    create_artworks_table funcation created.

--- db.py
+++ db.py
@@ -2,7 +2,12 @@ import sqlite3 # Import SQLite3
 
 database = "art.sqlite3"
 
-def create_artist_table():
+def create_artists_table():
     with sqlite3.connect("art.qlite3") as conn:
-        conn.execute("CREATE TABLE IF NOT EXISTS artists (artist_name TEXT, artist_email TEXT(UNIQUE artist_name COLLATE NOCASE, artist_email COLLATE NOCASE))")
+        conn.execute("CREATE TABLE IF NOT EXISTS artists (artist_name TEXT, artist_email TEXT (UNIQUE artist_email COLLATE NOCASE))")
+    conn.close()
+
+def create_artworks_table():
+    with sqlite3.connect("art.qlite3") as conn:
+        conn.execute("CREATE TABLE IF NOT EXISTS artworks (artist_name TEXT, artwork_name TEXT, price REAL, available BOOLEAN (UNIQUE artwork_name COLLATE NOCASE))")
     conn.close()
