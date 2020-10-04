503699bb88b10bed877577f5f53926b711078e31
“jy8474np” <jy8474np@go.minnstate.edu>
2020-10-04 17:26:57 -0500

    Menu and menu's while True loop in progress.

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import ui # Connect to ui.py module
 import db # Connect to db.py module
+
+while True:
+    print('Please select an option from the menu below. Enter "Q" to quit.')
+    print("1. Add Artist\n2. Add Artwork\n3. Search by Artist\n4. Delete Artwork\n5. Display Artist's Work\n6. Change Artwork Availablity")
+
+    option = input("Enter your desired option: ")
+
+    if option == "1":
+        add_new_artist()
+    elif task == "2":
+        
