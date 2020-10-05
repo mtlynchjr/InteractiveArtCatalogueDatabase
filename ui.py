10b956e9b536669e3af403e6b611a1d951d1758f
“jy8474np” <jy8474np@go.minnstate.edu>
2020-10-04 20:54:14 -0500

    ui.py template started.

---
+++ ui.py
@@ -0,0 +1,7 @@
+def get_non_empty_string(question):
+    # TO DO: Validation
+    return input(question)
+
+def get_positive_float(question):
+    # TO DO: Validation
+    return float(input(question))
