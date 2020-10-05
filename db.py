2e3935a33c874882f6df38666f4b58e29a56c043
“jy8474np” <jy8474np@go.minnstate.edu>
2020-10-04 23:10:27 -0500

    final main commit before push and merge.

--- db.py
+++ db.py
@@ -1,46 +1,46 @@
 import sqlite3 # Import SQLite3
-import main
+import main # Import main.py
 
 database = "art.sqlite3" # For ease, make "database" a Global Variable always associated with "art.sqlite"
 
+# Create Artists table function separately from Artworks table, if it doesn't already exist
 def create_artists_table():
     with sqlite3.connect(database) as conn:
-        conn.execute("CREATE TABLE IF NOT EXISTS artists (artist_name TEXT, artist_email TEXT)")
+        conn.execute("CREATE TABLE IF NOT EXISTS artists (artist_name TEXT, artist_email BLOB)")
     conn.close()
 
+# Create Artworks tale function separately from Artists table, if it doesn't already exist
 def create_artworks_table():
     with sqlite3.connect(database) as conn:
-        conn.execute("CREATE TABLE IF NOT EXISTS artworks (artwork_name TEXT, artist_name TEXT, price REAL, available TEXT")
+        conn.execute("CREATE TABLE IF NOT EXISTS artworks (artwork_name TEXT, artist_name TEXT, price REAL, available TEXT)")
     conn.close()
 
+# Create tables
 create_artists_table()
 create_artworks_table()
 
+# Using values from main.py update database with a new artist
 def add_new_artist(new_artist_name, new_artist_email):
-    # new_artist_name = input("What is the name of the artist you would like to add? ") # Prompt useer for new artist's name
-    # new_artist_email = input("Please enter the e-mail address of the artist you would like to add: ") # Prompt user for new artist's e-mail address
+    new_artist_name = main.add_new_artist
     with sqlite3.connect(database) as conn:
         conn.execute(f"INSERT INTO artists VALUES (? , ?)", (new_artist_name , new_artist_email))
     conn.close()
 
+# Using values from main.py update database with a new artwork
 def add_new_artwork(new_artwork, new_artist, new_price, new_avaiability):
-    # new_artwork = input("What is the name of the artwork you would like to add? ") # Prompt user for new artworks's name
-    # new_artist = input("What is the name of the artist attributed to that artwork? ") # Prompt user for new artwork's artist
-    # new_price = float(input("How much does the artwork cost? $")) # Prompt user for new artwork's price.
-    # new_avaiability = input("Is the artwork currently available for purchase? ") # Prompt user for new artwork's availability
     with sqlite3.connect(database) as conn:
         conn.execute(f"INSERT INTO artworks VALUES (? , ?, ?, ?)", (new_artwork, new_artist, new_price, new_avaiability))
     conn.close()
 
+# Using values from main.py update database with the name of the artists whose artworks are to be displayed
 def search_by_artist(search_artist):
-    # search_artist = input("What is the name of the artist whose artworks you want to see? ")
     with sqlite3.connect(database) as conn:
         results = conn.execute("SELECT artwork_name FROM artworks WHERE artist_name like ?", (search_artist, ))
         return(results)
     conn.close()
 
+# Using values from main.py update database with the name of the artwork to be deleted
 def delete_artwork(deleted_artwork):
-    # deleted_artwork = input("What is the name of the artwork you would like to remove? ")
     with sqlite3.connect(database) as conn:
         conn.execute(f"DELETE FROM artworks WHERE artwork_name = ?", (deleted_artwork, ))
     conn.close()
@@ -50,21 +50,12 @@ def delete_artwork(deleted_artwork):
         # with sqlite3.connect(database) as conn:
         # results = conn.execute("SELECT available FROM artworks WHERE artwork_name = ?", (available_update, ))
         # if results = True:
-    
         #return(results)
     #conn.close()
 
+# Using values from main.py update database with an artist's available artwork
 def available_artwork(available_artist):
-    #available_artist = input("What is the name of the artist whose available artworks you want to see? ")
     with sqlite3.connect(database) as conn:
         results = conn.execute("SELECT artwork_name FROM artworks WHERE artist_name = ? AND available IS TRUE", (available_artist, ))
         return(results)
     conn.close()
-
-#create_artists_table()
-# create_artworks_table()
-# add_new_artist()
-# add_new_artwork()
-# search_by_artist()
-# delete_artwork()
-# available_artwork()
