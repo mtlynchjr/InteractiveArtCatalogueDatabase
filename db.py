9f606551ab12b7283f3e55cb177233a806e7f430
“jy8474np” <jy8474np@go.minnstate.edu>
2020-10-04 21:48:38 -0500

    Moved inputs to main.py, included validation tests from ui.py in user input prompts, and updated db.py to reflect main.py variables instead of inputs,

--- db.py
+++ db.py
@@ -10,61 +10,61 @@ def create_artists_table():
 
 def create_artworks_table():
     with sqlite3.connect(database) as conn:
-        conn.execute("CREATE TABLE IF NOT EXISTS artworks (artwork_name TEXT, artist_name TEXT, price REAL, available BOOLEAN")
+        conn.execute("CREATE TABLE IF NOT EXISTS artworks (artwork_name TEXT, artist_name TEXT, price REAL, available TEXT")
     conn.close()
 
-def add_new_artist():
+create_artists_table()
+create_artworks_table()
+
+def add_new_artist(new_artist_name, new_artist_email):
     # new_artist_name = input("What is the name of the artist you would like to add? ") # Prompt useer for new artist's name
     # new_artist_email = input("Please enter the e-mail address of the artist you would like to add: ") # Prompt user for new artist's e-mail address
-    
     with sqlite3.connect(database) as conn:
         conn.execute(f"INSERT INTO artists VALUES (? , ?)", (new_artist_name , new_artist_email))
     conn.close()
 
-def add_new_artwork():
-    new_artwork = input("What is the name of the artwork you would like to add? ") # Prompt user for new artworks's name
-    new_artist = input("What is the name of the artist attributed to that artwork? ") # Prompt user for new artwork's artist
-    new_price = float(input("How much does the artwork cost? $")) # Prompt user for new artwork's price.
-    new_avaiability = input("Is the artwork currently available for purchase? ") # Prompt user for new artwork's availability
+def add_new_artwork(new_artwork, new_artist, new_price, new_avaiability):
+    # new_artwork = input("What is the name of the artwork you would like to add? ") # Prompt user for new artworks's name
+    # new_artist = input("What is the name of the artist attributed to that artwork? ") # Prompt user for new artwork's artist
+    # new_price = float(input("How much does the artwork cost? $")) # Prompt user for new artwork's price.
+    # new_avaiability = input("Is the artwork currently available for purchase? ") # Prompt user for new artwork's availability
     with sqlite3.connect(database) as conn:
         conn.execute(f"INSERT INTO artworks VALUES (? , ?, ?, ?)", (new_artwork, new_artist, new_price, new_avaiability))
     conn.close()
 
-def search_by_artist():
-    search_artist = input("What is the name of the artist whose artworks you want to see? ")
+def search_by_artist(search_artist):
+    # search_artist = input("What is the name of the artist whose artworks you want to see? ")
     with sqlite3.connect(database) as conn:
         results = conn.execute("SELECT artwork_name FROM artworks WHERE artist_name like ?", (search_artist, ))
         return(results)
     conn.close()
 
-def delete_artwork():
-    delete_artwork = input("What is the name of the artwork you would like to remove? ")
+def delete_artwork(deleted_artwork):
+    # deleted_artwork = input("What is the name of the artwork you would like to remove? ")
     with sqlite3.connect(database) as conn:
-        conn.execute(f"DELETE FROM artworks WHERE artwork_name = ?", (delete_artwork, ))
+        conn.execute(f"DELETE FROM artworks WHERE artwork_name = ?", (deleted_artwork, ))
     conn.close()
 
-# def change_availability():
+# def change_availability(available_update):
     # available_update = input("What is the name of the artwork whose availability you would like to update? ")
         # with sqlite3.connect(database) as conn:
         # results = conn.execute("SELECT available FROM artworks WHERE artwork_name = ?", (available_update, ))
         # if results = True:
-        #
+    
         #return(results)
     #conn.close()
 
-def available_artwork():
-    search_artist = input("What is the name of the artist whose available artworks you want to see? ")
+def available_artwork(available_artist):
+    #available_artist = input("What is the name of the artist whose available artworks you want to see? ")
     with sqlite3.connect(database) as conn:
-        results = conn.execute("SELECT artwork_name FROM artworks WHERE artist_name = ? AND available IS TRUE", (search_artist, ))
+        results = conn.execute("SELECT artwork_name FROM artworks WHERE artist_name = ? AND available IS TRUE", (available_artist, ))
         return(results)
     conn.close()
 
-# Code functions as-is when called
-
-create_artists_table()
-create_artworks_table()
-add_new_artist()
-add_new_artwork()
-search_by_artist()
-delete_artwork()
-available_artwork()
+#create_artists_table()
+# create_artworks_table()
+# add_new_artist()
+# add_new_artwork()
+# search_by_artist()
+# delete_artwork()
+# available_artwork()
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import ui # Connect to ui.py module
-from db import database # Connect to db.py module
+import db
 
 while True:
-
-    print('Please select an option from the menu below. Enter "Q" to quit.')
+    print("Welcome to the Interactive Art Catolouge!\n")
+    print('Please select an option from the menu below: (Enter "Q" to Quit)')
     print("1. Add Artist\n2. Add Artwork\n3. Search by Artist\n4. Delete Artwork\n5. Display Artist's Work\n6. Change Artwork Availablity\n7. Display Artist's Available Artwork")
 
     option = input("Enter your desired option: ")
@@ -16,24 +16,42 @@ while True:
         search_by_artist()
     elif option == "4":
         delete_artwork()
-    elif option == "5":
-        change_availability()
+    # elif option == "5":
+        # change_availability()
     elif option == "6":
         available_artwork()
     elif option == "q" or option == "Q":
-        break
-    else:
-        print("You did not choose a valid option.")
+        good_bye()
         break
 
 def add_new_artist():
     new_artist_name = ui.get_non_empty_string(input("What is the name of the artist you would like to add? ")) # Prompt useer for new artist's name
-    # new_artist_name = ui.get_non_empty_string()
+    new_artist_email = ui.get_non_empty_string(input("Please enter the e-mail address of the artist you would like to add: ")) # Prompt useer for new artist's e-mail address
+    db.add_new_artist(new_artist_name, new_artist_email)
 
-# add_new_artist()
-# add_new_artwork()
-# search_by_artist()
-# delete_artwork()
-# display_artist_artwork()
-# change_availability()
-# display_available_artwork()
+def add_new_artwork():
+    new_artwork = ui.get_non_empty_string(input("What is the name of the artwork you would like to add? ")) # Prompt user for new artworks's name
+    new_artist = ui.get_non_empty_string(input("What is the name of the artist attributed to that artwork? ")) # Prompt user for new artwork's artist
+    new_price = ui.get_positive_float(float(input("How much does the artwork cost? $"))) # Prompt user for new artwork's price.
+    new_avaiability = ui.get_non_empty_string(input("Is the artwork currently available for purchase? ")) # Prompt user for new artwork's availability
+    db.add_new_artwork(new_artwork, new_artist, new_price, new_avaiability)
+
+def search_by_artist():
+    search_artist = ui.get_non_empty_string(input("What is the name of the artist whose available artworks you want to see? "))
+    db.search_by_artist(search_artist, )
+
+def delete_artwork():
+    deleted_artwork = ui.get_non_empty_string(input("What is the name of the artwork you would like to remove? "))
+    db.delete_artwork(deleted_artwork, )
+
+# def change_availability():
+    # available_update = ui.VALIDATION(input("What is the name of the artwork whose availability you would like to update? "))
+    # db.change_availability(availabile_update, )
+
+def available_artwork():
+    available_artist = ui.get_non_empty_string(input("What is the name of the artist whose available artworks you want to see? "))
+    db.available_artwork(available_artist, )
+
+def good_bye():
+    print("Good-bye!")
+
